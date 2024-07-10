c404a410305a114b570619a028717f62cf2253aa
Jorbaez <jorgelinabaez1@hotmail.com>
2024-07-10 20:27:01 -0300

    de vuelta a lo anterior

--- config/database.js
+++ config/database.js
@@ -1,33 +1,33 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('wewashdb', 'root', 'password', {
+host: 'localhost',
+dialect: 'mysql'
+ });
+
+ sequelize.authenticate()
+     .then(() => console.log('Database connected...'))
+     .catch(err => console.log('Error: ' + err));
+
+ module.exports = sequelize;
+
+
 // const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('wewashdb', 'root', 'password', {
-//     host: 'localhost',
-//     dialect: 'mysql'
+// const sequelize = new Sequelize('368243_wewash  ', 'jorgelinabaez', 'cacdeploy', {
+//   host: 'jorgelinabaez.alwaysdata.net',
+//   dialect: 'mysql',
+//   port: 3001, // Este es el puerto predeterminado para MySQL
 // });
 
 // sequelize.authenticate()
-//     .then(() => console.log('Database connected...'))
-//     .catch(err => console.log('Error: ' + err));
+//   .then(() => {
+//     console.log('La conexión se ha establecido exitosamente.');
+//   })
+//   .catch(err => {
+//     console.error('No se puede conectar a la base de datos:', err);
+//   });
 
-// module.exports = sequelize;
-
-
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('368243_wewash  ', 'jorgelinabaez', 'cacdeploy', {
-  host: 'jorgelinabaez.alwaysdata.net',
-  dialect: 'mysql',
-  port: 3001, // Este es el puerto predeterminado para MySQL
-});
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('La conexión se ha establecido exitosamente.');
-  })
-  .catch(err => {
-    console.error('No se puede conectar a la base de datos:', err);
-  });
-
-  module.exports = sequelize;
+//   module.exports = sequelize;
 
   
