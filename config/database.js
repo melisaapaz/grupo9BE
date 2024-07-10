c5043c36089213d2c492b457c50f78da82aee425
Jorbaez <jorgelinabaez1@hotmail.com>
2024-07-10 20:09:55 -0300

    cambios databasejs

--- config/database.js
+++ config/database.js
@@ -1,12 +1,33 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('wewashdb', 'root', 'password', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+// sequelize.authenticate()
+//     .then(() => console.log('Database connected...'))
+//     .catch(err => console.log('Error: ' + err));
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('wewashdb', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize('jorgelinabaez_wewash ', 'jorgelinabaez', 'cacdeploy', {
+  host: 'jorgelinabaez.alwaysdata.net',
+  dialect: 'mysql',
+  port: 3306, // Este es el puerto predeterminado para MySQL
 });
 
 sequelize.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('Error: ' + err));
+  .then(() => {
+    console.log('La conexión se ha establecido exitosamente.');
+  })
+  .catch(err => {
+    console.error('No se puede conectar a la base de datos:', err);
+  });
 
-module.exports = sequelize;
+  module.exports = sequelize;
+
+  
